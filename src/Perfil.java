a5ad895724afd8d219de3f673169ae25fb9829e4
Leonardo <leonardo38361@gmail.com>
2023-11-13 18:02:37 -0300

    Creacion de la clase Perfil

--- src/Perfil.java
+++ src/Perfil.java
@@ -1,2 +1,29 @@
 public class Perfil {
+    private int id;
+    private String sexo;
+    private String direccion;
+    private String formacion;
+    private String estadoCivil;
+    private String fotoPerfil;
+    private String biografia;
+
+    public boolean actualizarPerfil() {
+        return true;
+    }
+
+    public boolean enviarSolicitud(int solicitud) {
+        return true;
+    }
+
+    public int aceptarSolicitud(int solicitud) {
+        return solicitud;
+    }
+
+    public int eliminarAmigo(int idAmigo) {
+        return idAmigo;
+    }
+
+    public int rechazarSolicitud(int idAmigoDestino) {
+        return idAmigoDestino;
+    }
 }
