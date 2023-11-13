f31c7842f4d8611a9270e959625cf2bd3aaf05ab
Leonardo <leonardo38361@gmail.com>
2023-11-13 19:59:26 -0300

    creación de metodos y atributos

--- src/Registro.java
+++ src/Registro.java
@@ -1,2 +1,46 @@
 public class Registro {
+    private int id;
+    private String nombre;
+    private String Apodo;
+    private int telefono;
+    private int edad;
+
+    public Registro(String nombre, String apodo, int telefono, int edad) {
+        this.nombre = nombre;
+        Apodo = apodo;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApodo() {
+        return Apodo;
+    }
+
+    public void setApodo(String apodo) {
+        Apodo = apodo;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 }
