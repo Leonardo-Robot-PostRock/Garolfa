f79790fe9db9ed09d948dbb6b347f3eeaa0bf8b4
Leonardo <leonardo38361@gmail.com>
2023-11-13 17:48:13 -0300

    atributos y metodos en Comentario

--- src/Comentario.java
+++ src/Comentario.java
@@ -1,2 +1,23 @@
 public class Comentario {
+    private int id;
+    private String comentario;
+    private String fechaDeCreacion;
+    private int idPublicacion;
+    private int idCuenta;
+
+    public boolean enviarComentario(){
+        return true;
+    }
+
+    public boolean eliminarComentario() {
+        return true;
+    }
+
+    public boolean meGustaComentario(){
+        return true;
+    }
+
+    public boolean noMeGustaComentario(){
+        return true;
+    }
 }
