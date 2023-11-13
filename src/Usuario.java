2b34b32575294aa5bc6c331024814e4b9d46eb79
Leonardo <leonardo38361@gmail.com>
2023-11-13 19:59:59 -0300

    atributos y métodos, hereda de Registro, creacion de constructores

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,2 +1,23 @@
-public class Usuario {
+public class Usuario extends Registro {
+    private String id;
+    private String password;
+    private String correo;
+
+    public Usuario(String nombre, String apodo, int telefono, int edad, String correo) {
+        super(nombre, apodo, telefono, edad);
+        this.correo = correo;
+    }
+
+
+    public void login() {
+
+    }
+
+    public boolean verificar() {
+        return true;
+    }
+
+    public void registrarse() {
+
+    }
 }
