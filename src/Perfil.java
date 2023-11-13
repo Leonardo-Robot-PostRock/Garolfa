614ce4bed46408d09a3b769060b13bc29573a695
Leonardo <leonardo38361@gmail.com>
2023-11-13 19:58:35 -0300

    clase perfil hereda de usuario, implementacion de constructores

--- src/Perfil.java
+++ src/Perfil.java
@@ -1,4 +1,4 @@
-public class Perfil {
+public class Perfil extends Usuario{
     private int id;
     private String sexo;
     private String direccion;
@@ -6,6 +6,20 @@ public class Perfil {
     private String estadoCivil;
     private String fotoPerfil;
     private String biografia;
+
+    public Perfil(String nombre, String apodo, int telefono, int edad, String correo) {
+        super(nombre, apodo, telefono, edad, correo);
+    }
+
+    public Perfil(String nombre, String apodo, int telefono, int edad, String correo, String sexo, String direccion, String formacion, String estadoCivil, String fotoPerfil, String biografia) {
+        super(nombre, apodo, telefono, edad, correo);
+        this.sexo = sexo;
+        this.direccion = direccion;
+        this.formacion = formacion;
+        this.estadoCivil = estadoCivil;
+        this.fotoPerfil = fotoPerfil;
+        this.biografia = biografia;
+    }
 
     public boolean actualizarPerfil() {
         return true;
