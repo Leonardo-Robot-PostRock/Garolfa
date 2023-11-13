e828ebd86bd5062eb7e350d82945d3b0a3d71c32
Leonardo <leonardo38361@gmail.com>
2023-11-13 01:16:15 -0300

    atributos y metodos

--- src/Amigo.java
+++ src/Amigo.java
@@ -1,2 +1,16 @@
 public class Amigo {
+    private int idAmigo;
+    private String fechaDeAMistad;
+
+    public boolean enviarMensajes(){
+        return  true;
+    }
+
+    public boolean responderMensaje(){
+        return true;
+    }
+
+    public boolean eliminarMensaje(){
+        return true;
+    }
 }
