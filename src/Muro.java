38cda791234f65000c86a01a0625c756e409425a
Leonardo <leonardo38361@gmail.com>
2023-11-13 17:52:54 -0300

    creacion de atributos y metodos

--- src/Muro.java
+++ src/Muro.java
@@ -1,2 +1,13 @@
 public class Muro {
+    private int id;
+    private int perfil_id;
+    private Publicacion publicacion;
+
+    public Publicacion mostrarPublicaciones() {
+        return publicacion;
+    }
+
+    public Publicacion filtrarPublicaciones() {
+        return publicacion;
+    }
 }
