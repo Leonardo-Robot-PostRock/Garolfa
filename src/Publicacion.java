a8561cc83ba09bbfcfeb46afd3d568945e616f6e
Leonardo <leonardo38361@gmail.com>
2023-11-13 19:58:57 -0300

    atributos y métodos

--- src/Publicacion.java
+++ src/Publicacion.java
@@ -1,2 +1,19 @@
 public class Publicacion {
+    private int id;
+    private String titulo;
+    private String fechaDeCreacion;
+    private int fotoId;
+
+    public boolean meGustaPublicacion(){
+        return true;
+    }
+
+    public boolean quitarMeGustaDePublicacion(){
+        return true;
+    }
+
+    public void editarPublicacion(){
+
+    }
+
 }
