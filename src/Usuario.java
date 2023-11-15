e9d15b917fe327776bc23122fa8bd86e030a2608
Leonardo <leonardo38361@gmail.com>
2023-11-15 04:13:14 -0300

    cambio de tipo de datos

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,9 +1,11 @@
+import java.util.UUID;
+
 public class Usuario extends Registro {
-    private String id;
+    private int id;
     private String password;
     private String correo;
 
-    public Usuario(String nombre, String apodo, int telefono, int edad, String correo) {
+    public Usuario(String nombre, String apodo, long telefono, int edad, String correo) {
         super(nombre, apodo, telefono, edad);
         this.correo = correo;
     }
